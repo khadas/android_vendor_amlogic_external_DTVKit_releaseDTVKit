e30a5d688fb25e8ac2ede183cfb57b1a6bb9df42
junchao yuan <junchao.yuan@amlogic.com>
2018-11-22 15:35:45 +0800

    DtvKitTvinput: add interface for scanning Dtv [1/1]
    PD# SWPL-2256
    
    Problem:
    need support DVB-T/T2, DVB-S/S2
    
    Solution:
    add interface for scanning Dtv;
    
    Verify:
    None;
    
    Change-Id: I1d34b90ad7c15f624791e8983e3910b4db4fecbe
    Signed-off-by: junchao.yuan

---
+++ app/src/main/java/org/dtvkit/inputsource/DtvkitDvbScan.java
@@ -0,0 +1,271 @@
+package org.dtvkit.inputsource;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.tv.TvInputInfo;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import org.dtvkit.companionlibrary.EpgSyncJobService;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Locale;
+
+import android.media.tv.TvContract;
+
+public class DtvkitDvbScan {
+
+    private static final String TAG = "DtvkitDvbScan";
+    private ScannerEventListener mScannerListener = null;
+
+    public final static int EVENT_SCAN_PROGRESS             = 0;
+    public final static int EVENT_STORE_BEGIN               = 1;
+    public final static int EVENT_STORE_END                 = 2;
+    public final static int EVENT_SCAN_END                  = 3;
+    public final static int EVENT_BLINDSCAN_PROGRESS        = 4;
+    public final static int EVENT_BLINDSCAN_NEWCHANNEL      = 5;
+    public final static int EVENT_BLINDSCAN_END             = 6;
+    public final static int EVENT_ATV_PROG_DATA             = 7;
+    public final static int EVENT_DTV_PROG_DATA             = 8;
+    public final static int EVENT_SCAN_EXIT                 = 9;
+    public final static int EVENT_SCAN_BEGIN                = 10;
+    public final static int EVENT_LCN_INFO_DATA             = 11;
+
+    public class ScannerEvent {
+        public int type;
+        public int precent;
+        public int totalcount;
+    }
+
+    public interface ScannerEventListener {
+        void onEvent(ScannerEvent ev);
+    }
+    //scanner
+    public void setScannerListener(ScannerEventListener l) {
+        mScannerListener = l;
+    }
+    /*
+    used for receive scan event
+     */
+    private final DtvkitGlueClient.SignalHandler mSignalEventHandler = new DtvkitGlueClient.SignalHandler() {
+        @Override
+        public void onSignal(String signal, JSONObject data) {
+
+            if (signal.equals("DvbtStatusChanged")) {
+                int progress = 0;
+                try {
+                    progress = data.getInt("progress");
+                } catch (JSONException ignore) {
+
+                }
+                Log.d(TAG, "progress:" + progress);
+                if (progress < 100) {
+                    int found = 0;
+                    try
+                    {
+                        JSONObject obj = DtvkitGlueClient.getInstance().request("Dvb.getNumberOfServices", new JSONArray());
+                        found = obj.getInt("data");
+                    }
+                    catch (Exception ignore)
+                    {
+                        Log.e(TAG, ignore.getMessage());
+                    }
+                    ScannerEvent scan_ev = new ScannerEvent();
+                    scan_ev.type = EVENT_SCAN_PROGRESS;
+                    scan_ev.precent = progress;
+                    scan_ev.totalcount = found;
+                    if (mScannerListener != null)
+                        mScannerListener.onEvent(scan_ev);
+                } else {
+                    // scan end, need stop dtvkit scan
+                    // Log.d(TAG, "scan start store db");
+                    ScannerEvent scan_ev = new ScannerEvent();
+                    scan_ev.type = EVENT_STORE_BEGIN;
+                    scan_ev.precent = progress;
+                    if (mScannerListener != null)
+                        mScannerListener.onEvent(scan_ev);
+                }
+            }
+        }
+    };
+     /*
+    used for sync tv.db, receiver sync event
+     */
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, final Intent intent) {
+            String status = intent.getStringExtra(EpgSyncJobService.SYNC_STATUS);
+            if (status == EpgSyncJobService.SYNC_FINISHED) {
+                // need stop sync
+                Log.d(TAG, "EpgSyncJobService:: SYNC_FINISHED");
+                // set scan status
+                ScannerEvent scan_ev = new ScannerEvent();
+                scan_ev.type = EVENT_SCAN_END;
+                if (mScannerListener != null)
+                    mScannerListener.onEvent(scan_ev);
+            }
+        }
+    };
+
+    private int startDvbtSearch() {
+        int ret = 0;
+        try {
+            JSONArray args = new JSONArray();
+            args.put(false); // Commit
+            DtvkitGlueClient.getInstance().request("Dvbt.finishSearch", args);
+        } catch (Exception e) {
+            ret = -1;
+            return ret;
+        }
+
+        try {
+            JSONArray args = new JSONArray();
+            args.put(true); // retune
+            DtvkitGlueClient.getInstance().request("Dvbt.startSearch", args);
+        } catch (Exception e) {
+            ret = -1;
+            return ret;
+        }
+        return ret;
+    }
+    private int stopDvbtSearch() {
+        int ret = 0;
+        try {
+            JSONArray args = new JSONArray();
+            args.put(true); // Commit
+            DtvkitGlueClient.getInstance().request("Dvbt.finishSearch", args);
+        } catch (Exception e) {
+            ret = -1;
+            return ret;
+        }
+        return ret;
+    }
+
+    private int stopDvbsSearch() {
+        int ret = 0;
+        try {
+            JSONArray args = new JSONArray();
+            args.put(true); // Commit
+            DtvkitGlueClient.getInstance().request("Dvbs.finishSearch", args);
+        } catch (Exception e) {
+            ret = -1;
+            return ret;
+        }
+        return ret;
+    }
+
+    public int startSearch(String protocol, JSONObject scanParam) {
+        int ret = 0;
+        if (protocol == null || mSignalEventHandler == null) {
+            ret = -1;
+            Log.d(TAG, "startsearch error protocol or mHandler is null");
+            return ret;
+        }
+
+        switch (protocol) {
+            case TvContract.Channels.TYPE_DVB_S:
+                startMonitoringSearch(mSignalEventHandler);
+                ret = startDvbtSearch();
+                break;
+            case TvContract.Channels.TYPE_DVB_T:
+                startMonitoringSearch(mSignalEventHandler);
+                ret = startDvbtSearch();
+                break;
+            default:
+                // TODO: scan all for real
+                ret = -1;
+                Log.d(TAG, "startsearch error protocol not found");
+                break;
+        }
+        if (ret != 0) {
+            Log.d(TAG, "startsearch error");
+        }
+        ScannerEvent scan_ev = new ScannerEvent();
+        scan_ev.type = EVENT_SCAN_BEGIN;
+        if (mScannerListener != null)
+            mScannerListener.onEvent(scan_ev);
+        return ret;
+    }
+
+    public int stopScan(String protocol) {
+        int ret = 0;
+        stopMonitoringSearch(mSignalEventHandler);
+        switch (protocol) {
+            case TvContract.Channels.TYPE_DVB_S:
+                stopDvbsSearch();
+                break;
+            case TvContract.Channels.TYPE_DVB_T:
+                stopDvbtSearch();
+                break;
+            default:
+                // TODO: scan all for real
+                ret = -1;
+                Log.d(TAG, "stopScan error protocol not found");
+                break;
+        }
+        return ret;
+    }
+
+    public int StartSyncDb(Context context, String inputId) {
+        int ret = 0;
+        if (inputId == null || context == null) {
+            ret = -1;
+            return ret;
+        }
+        startMonitoringSync(context);
+        // By default, gets all channels and 1 hour of programs (DEFAULT_IMMEDIATE_EPG_DURATION_MILLIS)
+        EpgSyncJobService.cancelAllSyncRequests(context);
+        Log.d(TAG, String.format("StartSyncDb inputId: %s", inputId));
+        EpgSyncJobService.requestImmediateSync(context, inputId, 1000 * 60 * 60 * 12, true, new ComponentName(context, DtvkitEpgSync.class)); // 12 hours
+        return ret;
+    }
+
+    public int StopSyncDb(Context context) {
+        int ret = 0;
+        if (context == null) {
+            ret = -1;
+            return ret;
+        }
+        stopMonitoringSync(context);
+        EpgSyncJobService.cancelAllSyncRequests(context);
+        return ret;
+    }
+    private int startMonitoringSearch(DtvkitGlueClient.SignalHandler mHandler) {
+        int ret = 0;
+        if (mHandler == null) {
+            ret = -1;
+            Log.d(TAG, "stopMonitoringSearch error, mHandler is null");
+            return ret;
+        }
+        DtvkitGlueClient.getInstance().registerSignalHandler(mHandler);
+        return ret;
+    }
+
+    private int stopMonitoringSearch(DtvkitGlueClient.SignalHandler mHandler) {
+        int ret = 0;
+        if (mHandler == null) {
+            ret = -1;
+            Log.d(TAG, "stopMonitoringSearch error, mHandler is null");
+            return ret;
+        }
+        DtvkitGlueClient.getInstance().unregisterSignalHandler(mHandler);
+        return ret;
+    }
+
+    private void startMonitoringSync(Context context) {
+        LocalBroadcastManager.getInstance(context).registerReceiver(mReceiver,
+                new IntentFilter(EpgSyncJobService.ACTION_SYNC_STATUS_CHANGED));
+    }
+
+    private void stopMonitoringSync(Context context) {
+        LocalBroadcastManager.getInstance(context).unregisterReceiver(mReceiver);
+    }
+}
--- app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
+++ app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.util.LongSparseArray;
 import android.view.Surface;
@@ -23,6 +24,13 @@ import org.dtvkit.companionlibrary.utils.TvContractUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+/*
+dtvkit
+ */
+import org.dtvkit.inputsource.DtvkitDvbScan;
+import org.dtvkit.inputsource.DtvkitDvbScan.ScannerEvent;
+
+//import com.droidlogic.app.tv.TvControlManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +40,14 @@ public class DtvkitTvInput extends TvInputService {
     private LongSparseArray<Channel> mChannels;
     private ContentResolver mContentResolver;
     private DtvkitEpgTimer mEpgTimer = null;
+    protected DtvkitDvbScan mDtvkitDvbScan = null;
+    protected String mInputId = null;
+    protected String mProtocol = null;
+   // private TvControlManager Tcm = null;
+    private DtvScanHandler mDtvScanHandler;
+    private static final int MSG_DO_TRY_SCAN = 0;
+    private static final int RETRY_TIMES = 10;
+    private int retry_times = RETRY_TIMES;
 
     public DtvkitTvInput() {
         Log.i(TAG, "DtvkitTvInput");
@@ -42,9 +58,12 @@ public class DtvkitTvInput extends TvInputService {
         super.onCreate();
         Log.i(TAG, "onCreate");
         mEpgTimer = new DtvkitEpgTimer(getApplicationContext());
+        mDtvkitDvbScan = new DtvkitDvbScan();
+        setInputId(".DtvkitTvInput");
         mContentResolver = getContentResolver();
         mContentResolver.registerContentObserver(TvContract.Channels.CONTENT_URI, true, mContentObserver);
         onChannelsChanged();
+        //Tcm = new TvControlManager.getInstance();
     }
 
     @Override
@@ -62,10 +81,16 @@ public class DtvkitTvInput extends TvInputService {
         return new DtvkitTvInputSession(this);
     }
 
+    protected void setInputId(String name) {
+        ComponentName component = new ComponentName("org.dtvkit.inputsource", name);
+        mInputId = component.flattenToShortString();
+        Log.d(TAG, "set input id to " + mInputId);
+    }
+
     // We do not indicate recording capabilities. TODO for recording.
     //public TvInputService.RecordingSession onCreateRecordingSession(String inputId)
 
-    class DtvkitTvInputSession extends TvInputService.Session {
+    class DtvkitTvInputSession extends TvInputService.Session implements DtvkitDvbScan.ScannerEventListener {
         private static final String TAG = "DtvkitTvInputSession";
         private Channel mTunedChannel;
         private List<TvTrackInfo> mTunedTracks = null;
@@ -74,6 +99,7 @@ public class DtvkitTvInput extends TvInputService {
             super(context);
             Log.i(TAG, "DtvkitTvInputSession");
             DtvkitGlueClient.getInstance().registerSignalHandler(mHandler);
+            mDtvkitDvbScan.setScannerListener(this);
             setOverlayViewEnabled(false);
         }
 
@@ -81,6 +107,7 @@ public class DtvkitTvInput extends TvInputService {
             Log.i(TAG, "onRelease");
             playerStop();
             DtvkitGlueClient.getInstance().unregisterSignalHandler(mHandler);
+            mDtvkitDvbScan.setScannerListener(null);
         }
 
         @Override
@@ -167,6 +194,37 @@ public class DtvkitTvInput extends TvInputService {
         @Override
         public void onAppPrivateCommand(String action, Bundle data) {
             Log.i(TAG, "onAppPrivateCommand " + action + ", " + data);
+        }
+
+        @Override
+        public void onEvent(ScannerEvent event) {
+            switch (event.type) {
+                case DtvkitDvbScan.EVENT_SCAN_BEGIN:
+                    Log.d(TAG, "scan start");
+                    //mStatus = ScanStatusType.IN_PROG;
+                   // updateStatus(mStatus);
+                    break;
+                case DtvkitDvbScan.EVENT_SCAN_END:
+                    //mStatus = ScanStatusType.COMPLETE;
+                    //updateStatus(mStatus);
+                    Log.d(TAG, "scan end");
+                    DtvkitStopSyncDb();
+                    break;
+                case DtvkitDvbScan.EVENT_SCAN_PROGRESS:
+                    //mComplete = event.precent;
+                    //mDtvCount = event.totalcount;
+                    //updateChannelCount(ChannelType.DTV, mDtvCount);
+                    //updateProgress(mComplete);
+                    break;
+                case DtvkitDvbScan.EVENT_STORE_BEGIN:
+                    Log.d(TAG, "store begin");
+                    //amlDtvkitStopScan(mProtocol);
+                    DtvkitStartSyncDb(mInputId);
+                    break;
+                default:
+                    Log.d(TAG, "unhandled scanner event: " + event.type);
+                    break;
+            }
         }
 
         //public void onTimeShiftPlay(Uri recordedProgramUri)
@@ -412,4 +470,195 @@ public class DtvkitTvInput extends TvInputService {
             onChannelsChanged();
         }
     };
+
+    public void cancelScan() {
+        Log.d(TAG, "cancel scan");
+
+        /*
+        add dtvkit stop scan api, and unregist handle
+         */
+        DtvkitStopScan(mProtocol);
+        //mStatus = ScanStatusType.USER_ABORT;
+        //updateStatus(mStatus);
+    }
+
+    public int getScanStatus() {
+        Log.d(TAG, "get scan status");
+
+        //mCounts.put(ChannelType.ATV, mAtvCount);
+        //mCounts.put(ChannelType.DTV, mDtvCount);
+        //mCounts.put(ChannelType.IP, mIpCount);
+
+        return 0;//new ScanStatus(mComplete, mStatus, mCounts);
+    }
+
+    private void reset() {
+        Log.d(TAG, "reset");
+        //mStatus = ScanStatusType.NO_SCAN;
+        //mComplete = 0;
+        //mAtvCount = 0;
+        //mDtvCount = 0;
+        //mIpCount = 0;
+        //mCounts.clear();
+    }
+
+    public  class ScanType {
+        public static final int SCAN_DTV_AUTO = 0x1;
+        public static final int SCAN_DTV_MANUAL = 0x2;
+        public static final int SCAN_DTV_ALLBAND = 0x3;
+        public static final int SCAN_DTV_NONE = 0x7;
+
+        public static final int SCAN_ATV_AUTO = 0x1;
+        public static final int SCAN_ATV_MANUAL = 0x2;
+        public static final int SCAN_ATV_FREQ = 0x3;
+        public static final int SCAN_ATV_NONE = 0x7;
+
+        public static final int SCAN_ATV_AUTO_FREQ_LIST = 0x0; /* 0: freq table list sacn mode */
+        public static final int SCAN_ATV_AUTO_ALL_BAND = 0x1;  /* 1: all band sacn mode */
+
+        public ScanType() {}
+    }
+
+
+   public enum ScanProtocol {
+        AIR(0),
+        CABLE(1),
+        ALL(2);
+
+        private int val;
+
+        ScanProtocol(int val) {
+            this.val = val;
+        }
+
+        public int toInt() {
+            return this.val;
+        }
+    }
+
+    public void startAutoScan(ScanProtocol protocol, ScanType type) {
+        /*
+        need add check dtvkit is playing or stoping, if true, we need retry
+         */
+        if (false) {
+            Log.d(TAG, "startAutoScan block, wait for=" + retry_times + ", timeout is 200ms");
+            if (retry_times > 0) {
+                doStartAutoScaninHandle(protocol, type);
+                retry_times--;
+                return ;
+            }
+        } else {
+            Log.d(TAG, "startAutoScan is ok");
+            doStartAutoScan(protocol, type);
+        }
+    }
+
+    private final class DtvScanHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            Log.d(TAG, "AmlTvScanHandler, msg.what=" + message.what);
+            switch (message.what) {
+                case MSG_DO_TRY_SCAN:
+                    mDtvScanHandler.removeMessages(MSG_DO_TRY_SCAN);
+                    SomeArgs args = (SomeArgs) message.obj;
+                    startAutoScan((ScanProtocol)args.arg1, (ScanType)args.arg2);
+                    break;
+            }
+        }
+    }
+
+    private void doStartAutoScan(ScanProtocol protocol, ScanType type) {
+        reset();
+        String scanProtocol;
+        switch (protocol) {
+            case AIR:
+                scanProtocol = TvContract.Channels.TYPE_DVB_T;
+                break;
+            case CABLE:
+                scanProtocol = TvContract.Channels.TYPE_DVB_S;
+                break;
+            case ALL:
+                // TODO: scan all for real
+                scanProtocol = TvContract.Channels.TYPE_DVB_T;
+                break;
+            default:
+                scanProtocol = TvContract.Channels.TYPE_DVB_T;
+                break;
+        }
+
+        // start scan
+        /*
+        add dtvkit scan api and regist handle
+         */
+        mProtocol = scanProtocol;
+        Log.d(TAG, "doStartAutoScan");
+        DtvkitStartScan(mProtocol, null);
+        //mStatus = ScanStatusType.IN_PROG;
+        //updateStatus(mStatus);
+    }
+
+    public final class SomeArgs {
+         public Object arg1;
+         public Object arg2;
+         public SomeArgs() {
+
+         }
+         public void clear() {
+             arg1 = null;
+             arg2 = null;
+         }
+     }
+
+    private void doStartAutoScaninHandle(ScanProtocol protocol, ScanType type) {
+        Log.d(TAG, "doStartAutoScaninHandle");
+        Message message = mDtvScanHandler.obtainMessage();
+        message.what = MSG_DO_TRY_SCAN;
+        SomeArgs args = new SomeArgs();
+        synchronized(this) {
+            args.arg1 = protocol;
+            args.arg2 = type;
+            message.obj = args;
+        }
+        mDtvScanHandler.sendMessageDelayed(message, 200);
+    }
+        /*
+        add dtvkit scan api
+     */
+    /*
+        protocol  : TvContract.Channels.TYPE_DVB_T or TvContract.Channels.TYPE_DVB_S
+        scanParam : scan param for dtvt or dvbs.
+     */
+    private int DtvkitStartScan(String protocol, JSONObject scanParam) {
+        int ret = 0;
+        Log.d(TAG, "DtvkitStartScan:"+ " protocol" + protocol);
+        //mDtvkitDvbScan.setScannerListener(this);
+        mDtvkitDvbScan.startSearch(protocol, scanParam);
+        return ret;
+    }
+     /*
+        protocol  : TvContract.Channels.TYPE_DVB_T or TvContract.Channels.TYPE_DVB_S
+        mHandler  : used to got scan event
+     */
+    private int DtvkitStopScan(String protocol) {
+        int ret = 0;
+        Log.d(TAG, "DtvkitStopScan:"+ " protocol" + protocol);
+        mDtvkitDvbScan.stopScan(protocol);
+        return ret;
+    }
+        /*
+        inputId  : used for store channel to tv.db, can not null
+     */
+    public int DtvkitStartSyncDb(String inputId) {
+        int ret = 0;
+        Log.d(TAG, "DtvkitStartSyncDb:"+ " inputId" + inputId);
+        ret = mDtvkitDvbScan.StartSyncDb(this, inputId);
+        return ret;
+    }
+        /*
+     */
+    public int DtvkitStopSyncDb() {
+        int ret = 0;
+        ret = mDtvkitDvbScan.StopSyncDb(this);
+        return ret;
+    }
 }
