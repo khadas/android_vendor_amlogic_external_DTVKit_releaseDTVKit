e26a16e1b3eae546e8b46eacf61e9b15ee2db2f1
kieth liu <kieth.liu@amlogic.com>
2018-12-28 06:26:49 -0500

    dtvkit-inpusource: build to product partition [1/1]
    PD#SWPL-2256
    
    Problem:
    need link system lib
    
    Solution:
    build to product partition
    
    Verify:
    verify by einstein
    
    Change-Id: I42aa11d58471c69a38d18522ea1a2c88de3845ac
    Signed-off-by: kieth liu <kieth.liu@amlogic.com>

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -29,6 +29,7 @@ LOCAL_JNI_SHARED_LIBRARIES := libplatform
 LOCAL_REQUIRED_MODULES := libplatform
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_PRODUCT_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
 include $(BUILD_PACKAGE)
 include $(call all-makefiles-under, $(LOCAL_PATH))
