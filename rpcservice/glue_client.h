a5f20ada90943a62a70951804fce4c4eff4ad53c
Luan Yuan <luan.yuan@amlogic.com>
2019-11-07 14:46:21 +0800

    releaseDTVKit: release dtvkit library for bug SWPL-14090 [1/1]
    PD#SWPL-14090
    
    Problem:
    update openlinux to the lastest.
    
    Solution:
    update openlinux to the lastest.
    
    Verify:
    no need.
    
    3d72ffc releaseDTVKit: release dtvkit library for bug SWPL-14090 [1/1]
    e633368 releaseDTVkit: update libdtvkitserver library [1/1]
    f8be6c6 releaseDTVkit: update libdtvkitserver library [1/1]
    7b7bf30 releaseDTVKit: update libdtvkitserver library [1/1]
    9829b35 releaseDTVKit: update libdtvkitserver library [1/1]
    a5a7839 releaseDTVKit: update libdtvkitserver library [1/1]
    e24cbed releaseDTVKit: update libdtvkitserver library [1/1]
    533ab75 releaseDTVKit: update libdtvkitserver library [1/1]
    e89c5f1 releaseDTVKit: update libdtvkitserver library [1/1]
    d5d4e6c releaseDTVKit: update libdtvkitserver library [1/1]
    2c496fb releaseDTVKit: update libdtvkitserver library [1/1]
    5155ca3 releaseDTVKit: update libdtvkitserver library [1/1]
    fef6527 releaseDTVKit: update libdtvkitserver library [1/1]
    a04a770 releaseDTVkit: update libdtvkitserver library [1/1]
    7b7d9bd releaseDTVKit: update libdtvkitserver library [1/1]
    
    Change-Id: Id1f0445e02e6743054c4bae925ddcfe393dba441
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- rpcservice/glue_client.h
+++ rpcservice/glue_client.h
@@ -2,8 +2,16 @@
 #define GULE_CLIENT_H
 
 #include <iostream>
+#include <gui/IGraphicBufferProducer.h>
+#include <gui/Surface.h>
+#include <android/native_window.h>
+#include <GLES2/gl2.h>
+
+#define NUM_BUFFS 2
+#define NUM_TEXTURES 3
+#define NUM_FBUFFS 1
+
 typedef void (*SIGNAL_CB)(const std::string &signal, const std::string &data);
-typedef void  (*DISPATCHDRAW_CB)(int32_t src_width, int32_t src_height, int32_t dst_x, int32_t dst_y, int32_t dst_width, int32_t dst_height, const uint8_t *data);
 // Singleton mode, only used by getInstance
 class Glue_client {
 
@@ -13,23 +21,45 @@ private:
 
 public:
     int setSignalCallback(SIGNAL_CB cb);
-    int setDisPatchDrawCallback(DISPATCHDRAW_CB cb);
     int RegisterRWSysfsCallback(void *readCb, void *writeCb);
     int UnRegisterRWSysfsCallback(void);
     int RegisterRWPropCallback(void *readCb, void *writeCb);
     int UnRegisterRWPropCallback(void);
     int addInterface(void);
+    int SetSurface(int path, void *surface);
     std::string request(const std::string &resource, const std::string &json);
+    void setIGraphicBufferProducer(android::sp<android::IGraphicBufferProducer>& bufferProducer);
     static Glue_client* getInstance()
     {
         return p_client;
     }
     void dispatchDraw(int32_t src_width, int32_t src_height, int32_t dst_x, int32_t dst_y, int32_t dst_width, int32_t dst_height, const uint8_t *data);
     void dispatchSignal(const std::string &signal, const std::string &data);
+    void dispatchOverlayCreate(int id, uint32_t width, uint32_t height);
+    void OverlayDisplay();
+    void OverlayUpdate(int id, int32_t dst_x, int32_t dst_y, int32_t dst_width, int32_t dst_height, const uint8_t *data);
 
 private:
     SIGNAL_CB signal_callback = NULL;
-    DISPATCHDRAW_CB dispatchDraw_callback = NULL;
+    android::sp<android::Surface> m_overlayproducersurface;
+    ANativeWindow_Buffer m_buffer;
+    ANativeWindow *m_window;
+    uint32_t m_screen_width;
+    uint32_t m_screen_height;
+    EGLDisplay m_display;
+    EGLContext m_eglContext;
+    EGLSurface m_eglSurface;
+    GLuint m_shaderProgram;
+    GLint m_texImgLocation;
+    GLuint m_bufferObjects[NUM_BUFFS];
+    GLuint m_textures[NUM_TEXTURES];
+    GLuint m_framebuffers[NUM_FBUFFS];
+    GLboolean m_texture_visible[NUM_TEXTURES];
+
+    void initEGL(void);
+    void initTextures(void);
+    void initTexture(int id, uint32_t width, uint32_t height);
+    void initDrawing(void);
 };
 // no need add lock for new client.we new client first
 Glue_client* Glue_client::p_client = new Glue_client();
